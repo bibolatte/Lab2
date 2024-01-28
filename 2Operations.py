b5cb0f76c64ea3d600d77149941b7047c36ad634
Bibolat <b_satey@kbtu.kz>
2024-01-28 18:54:19 +0600

    commit

--- 2Operations.py
+++ 2Operations.py
@@ -5,3 +5,9 @@ print(10/2) #divides
 fruits = ["apple", "banana"]
 if "apple" in fruits:
   print("Yes, apple is a fruit!") 
+
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
