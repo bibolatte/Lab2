97828c3e433a4e0066b5b8203b7a4646318e989b
Bibolat <b_satey@kbtu.kz>
2024-01-28 18:34:54 +0600

    commit

--- 1Bool.py
+++ 1Bool.py
@@ -2,9 +2,6 @@ print(10 > 9) #it's true
 print(10 == 9) #false
 print(10 < 9) #also false
 
-a=145
-b=14
-if b > a:
-    print("b is greater than a")
-else:
-    print("b is not greater than a")
+print(bool("abc")) #true
+
+print(bool(0)) #false
---
+++ 2Operations.py
@@ -0,0 +1,7 @@
+print(10*5) #multiplies
+
+print(10/2) #divides
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!") 
