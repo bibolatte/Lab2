2a4046ca4d98e511568c51e59a9c5d8dee78b5a1
Bibolat <b_satey@kbtu.kz>
2024-01-29 17:01:08 +0600

    commit

---
+++ 3Lists.py
@@ -0,0 +1,19 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0]="kiwi"
+
+fruits.append("orange")
+
+fruits.insert(1, "lemon")
+
+fruits.remove("banana")
+
+print(fruits[-1])
+
+fruits1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits1[2:5])
+
+fruits2 = ["apple", "banana", "cherry"]
+print(len(fruits2))
